f8706f234e67592b5246cc962bb533637fc40867
SallyTemple <pfende@gmail.com>
2018-04-29 23:29:08 -0700

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,22 +3,22 @@ var famousMovieStars = [
       {
             name: "MattDamon",
             image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Damon_cropped.jpg/250px-Damon_cropped.jpg",
-            response: ["1", "2", "1", "1", "1", "2", "3", "4", "1", "1"],
+            response: ["1", "2", "1", "1", "1", "2", "3", "4", "1", "3"],
       },
       {
             name: "DenzelWashington",
             image: "https://i.pinimg.com/originals/e1/75/57/e17557fdef2d137ff62eb35f568e19d1.jpg",
-            response: ["1", "1", "1", "1", "1", "1", "2", "3", "1", "1"],
+            response: ["1", "1", "1", "1", "1", "1", "2", "3", "1", "2"],
       },
       {
             name: "AntonioBanderas",
             image: "http://ngradio.gr/wp-content/uploads/2015/09/Spanish-actor-Antonio-Banderas-Curly-hair-Home-Decoration-Art-silk-fabric-cloth-canvas-poster-printing.jpg",
-            response:  ["1", "4", "3", "1", "2", "4", "4", "2", "2", "4"],
+            response:  ["1", "4", "3", "1", "2", "4", "4", "1", "3", "5"],
       },
       {
             name: "Popeye",
             image: "http://www.cartoonswallpapers.net/wallpapers/popeye-cartoon-image-wallpaper-sony-xperia-z3.png",
-            response: ["5", "3", "2", "2", "3", "3", "1", "1", "4", "2"],
+            response: ["5", "3", "2", "2", "3", "3", "1", "2", "1", "1"],
       },
 
 ];
