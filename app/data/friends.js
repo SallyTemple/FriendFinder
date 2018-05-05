4d7f99966d1abf40df81607ef34d2c1f58d1f194
SallyTemple <pfende@gmail.com>
2018-05-05 16:00:04 -0700

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 //Objects of movie stars
 var famousMovieStars = [
       {
-            name: "MattDamon",
+            names: "MattDamon",
             photos: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Damon_cropped.jpg/250px-Damon_cropped.jpg",
             scores: ["1", "2", "1", "1", "1", "2", "3", "4", "1", "3"],
       },
       {
-            name: "DenzelWashington",
+            names: "DenzelWashington",
             photos: "https://i.pinimg.com/originals/e1/75/57/e17557fdef2d137ff62eb35f568e19d1.jpg",
             scores: ["1", "1", "1", "1", "1", "1", "2", "3", "1", "2"],
       },
       {
-            name: "AntonioBanderas",
+            names: "AntonioBanderas",
             photos: "http://ngradio.gr/wp-content/uploads/2015/09/Spanish-actor-Antonio-Banderas-Curly-hair-Home-Decoration-Art-silk-fabric-cloth-canvas-poster-printing.jpg",
             scores:  ["1", "4", "3", "1", "2", "4", "4", "1", "3", "5"],
       },
