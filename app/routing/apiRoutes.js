dfdddbf37e4a28d211eca6c2520d04ed43000c05
SallyTemple <pfende@gmail.com>
2018-05-05 02:48:21 -0700

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,38 @@
-var movieStars = require("../data/friends");
-var path = require('path');
+var famousMovieStars = require("../data/friends.js");
 
-module.exports = function(app) {
+
+module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
-        res.json(movieStars);
+        res.json(famousMovieStars);
     });
 
     app.post("/api/friends", function (req, res) {
-        movieStars.push(req.body);
-        
-    });
+       var newApplicant = req.body.scores;
+        var applicantScores = [];
+        var newMatch= 0;
+        var pairedActor = 0;
+
+ for(var i=0; i < famousMovieStars.length; i++){
+    var diffInScores = 0;
+
+    for(var p = 0; p < newApplicant.length; p++){
+      diffInScores += (Math.abs(parseInt(famousMovieStars[i].scores[p]) - parseInt(newApplicant[p])));
+    }
+    applicantScores.push(diffInScores);
+  }
+
+  for(var i = 0; i < applicantScores.length; i++){
+    if(applicantScores[i] <= applicantScores[pairedActor]){
+      pairedActor = i;
+    }
+  }
+
+
+  var popularPair = famousMovieStars[pairedActor];
+  res.json(popularPair);
+
+  
+  famousMovieStars.push(req.body);
+});
 };
+    
