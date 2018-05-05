4077cc72a7a089fa163f541cc5e4d1e223bc289c
SallyTemple <pfende@gmail.com>
2018-05-05 02:24:56 -0700

    update

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require("path");
 
 // Sets up the Express App
 var app = express();
-var PORT = 8082;
+//var PORT = 8082;
+var PORT = process.env.PORT || 8082;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
