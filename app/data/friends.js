23e239d3d404d85449d194aee60b8a173864a086
SallyTemple <pfende@gmail.com>
2018-05-05 04:01:45 -0700

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@
 var famousMovieStars = [
       {
             name: "MattDamon",
-            photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Damon_cropped.jpg/250px-Damon_cropped.jpg",
+            photos: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Damon_cropped.jpg/250px-Damon_cropped.jpg",
             scores: ["1", "2", "1", "1", "1", "2", "3", "4", "1", "3"],
       },
       {
             name: "DenzelWashington",
-            photo: "https://i.pinimg.com/originals/e1/75/57/e17557fdef2d137ff62eb35f568e19d1.jpg",
+            photos: "https://i.pinimg.com/originals/e1/75/57/e17557fdef2d137ff62eb35f568e19d1.jpg",
             scores: ["1", "1", "1", "1", "1", "1", "2", "3", "1", "2"],
       },
       {
             name: "AntonioBanderas",
-            photo: "http://ngradio.gr/wp-content/uploads/2015/09/Spanish-actor-Antonio-Banderas-Curly-hair-Home-Decoration-Art-silk-fabric-cloth-canvas-poster-printing.jpg",
+            photos: "http://ngradio.gr/wp-content/uploads/2015/09/Spanish-actor-Antonio-Banderas-Curly-hair-Home-Decoration-Art-silk-fabric-cloth-canvas-poster-printing.jpg",
             scores:  ["1", "4", "3", "1", "2", "4", "4", "1", "3", "5"],
       },
       {
             name: "Popeye",
-            photo: "http://www.cartoonswallpapers.net/wallpapers/popeye-cartoon-image-wallpaper-sony-xperia-z3.png",
+            photos: "http://www.cartoonswallpapers.net/wallpapers/popeye-cartoon-image-wallpaper-sony-xperia-z3.png",
             scores: ["5", "3", "2", "2", "3", "3", "1", "2", "1", "1"],
       },
 
